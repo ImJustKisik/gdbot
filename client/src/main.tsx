84169fe60b5f5923e0804376df2a97218be398c8
Constable Glist <thebulkaman@gmail.com>
2025-12-20 23:44:51 +0200

    feat: init Sentry in React frontend and add vite-env.d.ts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from "@sentry/react";
 import App from './App'
 import './index.css'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, 
+  // Session Replay
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
